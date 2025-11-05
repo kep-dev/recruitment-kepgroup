36ecff970013b3faebf391d709dd2fc08b977868
MuhammadRidhoPrasetyo <muhammadridho228@gmail.com>
2025-11-05 11:52:59 +0800

    feat : test attempt widget

---
+++ app/Filament/Widgets/CorrectWidget.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\JobVacancy;
+use Filament\Widgets\Widget;
+use App\Models\ApplicantAnswer;
+use App\Models\ApplicantTestAttempt;
+use Livewire\Attributes\Computed;
+use App\Models\JobVacancyTestItem;
+
+class CorrectWidget extends Widget
+{
+    protected string $view = 'filament.widgets.correct-widget';
+
+    public $applicantTestAttemptId;
+
+    #[Computed]
+    public function correctAnswers()
+    {
+        return ApplicantAnswer::query()
+            ->where('applicant_test_attempt_id', $this->applicantTestAttemptId)
+            ->where('is_correct', true)
+            ->count();
+    }
+
+    #[Computed]
+    public function falseAnswers()
+    {
+        return ApplicantAnswer::query()
+            ->where('applicant_test_attempt_id', $this->applicantTestAttemptId)
+            ->where('is_correct', false)
+            ->count();
+    }
+
+    #[Computed]
+    public function noAnswers()
+    {
+        return ApplicantAnswer::query()
+            ->where('applicant_test_attempt_id', $this->applicantTestAttemptId)
+            ->whereNull('selected_choice_id')
+            ->count();
+    }
+
+    #[Computed]
+    public function totalQuestions()
+    {
+        $jobVacancyTestItemId = ApplicantTestAttempt::find($this->applicantTestAttemptId)->job_vacancy_test_item_id;
+        ds($jobVacancyTestItemId);
+        return JobVacancyTestItem::query()
+            ->where('id', $jobVacancyTestItemId)
+            ->first()
+            ->number_of_question;
+    }
+}

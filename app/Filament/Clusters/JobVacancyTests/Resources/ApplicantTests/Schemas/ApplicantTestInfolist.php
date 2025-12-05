52f1c198145071fecb027151604b5b8b7c5ee1e6
MuhammadRidhoPrasetyo <muhammadridho228@gmail.com>
2025-12-05 08:36:28 +0800

    feat : show tes potensi akademik di halaman wawancara

--- app/Filament/Clusters/JobVacancyTests/Resources/ApplicantTests/Schemas/ApplicantTestInfolist.php
+++ app/Filament/Clusters/JobVacancyTests/Resources/ApplicantTests/Schemas/ApplicantTestInfolist.php
@@ -3,6 +3,12 @@
 namespace App\Filament\Clusters\JobVacancyTests\Resources\ApplicantTests\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\RepeatableEntry;
+use Filament\Infolists\Components\RepeatableEntry\TableColumn;
 
 class ApplicantTestInfolist
 {
@@ -10,7 +16,30 @@ class ApplicantTestInfolist
     {
         return $schema
             ->components([
-                //
+                Tabs::make('Tabs')
+                    ->tabs([
+                        Tab::make('Potensi Dasar Akademik')
+                            ->schema([
+                                RepeatableEntry::make('attempts')
+                                    ->hiddenLabel()
+                                    ->table([
+                                        TableColumn::make('Ujian'),
+                                        TableColumn::make('Skor'),
+                                    ])
+                                    ->schema([
+                                        TextEntry::make('test.title'),
+                                        TextEntry::make('score'),
+                                    ])
+                                    ->columnSpanFull()
+                            ])
+                            ->columnSpanFull(),
+
+                        Tab::make('Psikotest')
+                            ->schema([
+                                // ...
+                            ]),
+                    ])
+                    ->columnSpanFull()
             ]);
     }
 }
